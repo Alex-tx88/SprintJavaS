a62d3c57a228c3a26a602c8ee272d160d0e2f32f
Alex-tx88 <alexteix230@gmail.com>
2026-05-23 16:41:06 -0300

    atualização

--- js/carousel.js
+++ js/carousel.js
@@ -9,16 +9,21 @@ let carouselArr = [];
 //class Carousel
 class Carousel {
 
-    
+    constructor(image, title, url){
+        this.image = image;
+        this.title = title;
+        this.url = url;
+
+    }
       
     static Start(arr){
         if(arr){
 
-            if(arr.length > 0){
+            if(arr && arr.length > 0){
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
                 Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },6000);
             }
             
         } else {
@@ -27,6 +32,47 @@ class Carousel {
     }
 
     static Next(){
-        
+        let itemAtual = carouselArr[Carousel._sequence];
+        let divCarousel = document.getElementById("carousel");
+        divCarousel.style.backgroundImage = "url('img/" + itemAtual.image + "')";
+        divCarousel.style.backgroundRepeat = "no-repeat";
+        divCarousel.style.backgroundSize = "cover";
+        divCarousel.style.backgroundPosition = "center";
+
+        let divTitle = document.getElementById("carousel-title");
+        divTitle.innerHTML = `<a href="${itemAtual.url}">${itemAtual.title}</a>`;
+        Carousel._sequence++;
+        if(Carousel._sequence >= Carousel._size){
+            Carousel._sequence = 0;
+        }
+
+        let btnnext = document.getElementById("next-btn");
+
+        btnnext.onclick = function(){
+            clearInterval(Carousel._interval);
+            Carousel.Next();
+        }
+        }
+
+    static Previous(){
+        let itemAtual = carouselArr[Carousel._sequence];
+        let divCarousel = document.getElementById("carousel");
+        divCarousel.style.backgroundImage = "url('img/" + itemAtual.image + "')";
+        divCarousel.style.backgroundRepeat = "no-repeat";
+        divCarousel.style.backgroundSize = "cover";
+        divCarousel.style.backgroundPosition = "center";
+        let divTitle = document.getElementById("carousel-title");
+        divTitle.innerHTML = `<a href="${itemAtual.url}">${itemAtual.title}</a>`;
+        Carousel._sequence--;
+        if(Carousel._sequence < 0){
+            Carousel._sequence = Carousel._size - 1;
+        let btnprev = document.getElementById("prev-btn");
+
+        btnprev.onclick = function(){
+            clearInterval(Carousel._interval);
+            Carousel.Previous();}
+        }
     }
+
+
 };
